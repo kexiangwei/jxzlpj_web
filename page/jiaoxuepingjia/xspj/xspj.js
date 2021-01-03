6cd02ce3a1de94f67044a047f841471110169cab
keXiangwei <15313725815@189.cn>
2021-01-03 20:58:54 +0800

    20210103

--- page/jiaoxuepingjia/xspj/xspj.js
+++ page/jiaoxuepingjia/xspj/xspj.js
@@ -34,166 +34,164 @@ layui.use(['layer','table','form'], function(){
                         '4.谢谢您对学校教学工作的支持与配合。<br/>' +
                         '</div>'
                 });
-
-                //数据表格
-                let datatable = table.render({
-                    id: guid() //设定一个id，防止重复弹出
-                    ,elem : '#datatable'
-                    ,height : 580
-                    ,url: requestUrl+'/xspj/getPageList.do'
-                    ,where:{
-                        "accountType":accountType,
-                        "userId":function () {
-                            return $.cookie('userId');
-                        }
-                    }
-                    ,request: {
-                        pageName: 'pageIndex'
-                        ,limitName: 'pageSize'
-                    }
-                    ,response: {
-                        statusCode: 200 //规定成功的状态码，默认：0
-                    }
-                    ,parseData: function(res){ //res 即为原始返回的数据
-                        return {
-                            "code": res.code, //解析接口状态
-                            "msg": res.msg, //解析提示文本
-                            "count": res.data.totalNum, //解析数据长度
-                            "data": res.data.pageList //解析数据列表
-                        };
-                    }
-                    ,cols : [[ //表头
-                        {type:'numbers', title:'序号', width:80, fixed: 'left'}
-                        ,{field:'courseName', title:'课程名称', width:180, sort:true, templet: function (data) {
-                                let html = '<a class="layui-btn layui-btn-normal layui-btn-xs" lay-event="view">查看</a>';
-                                if(data.isPj === 2){
-                                    html = '<a class="layui-btn layui-btn-primary layui-btn-xs" lay-event="pj">评教</a>';
-                                }
-                                $('#datatable_bar').html(html);
-                                return data.courseName;
-                            }}
-                        ,{field:'courseAttr', title:'课程性质', width:150, sort:true}
-                        ,{field:'stuScore', title:'学分', width:150, sort:true}
-                        ,{field:'stuHour', title:'学时', width:150, sort:true}
-                        ,{field:'majorName', title:'适用专业', width:180, sort:true}
-                        ,{field:'collegeName', title:'开课学院', width:180, sort:true}
-                        ,{fixed: 'right', width:80, align:'center', toolbar: '#datatable_bar'}
-                    ]]
-                    ,even: true //隔行背景
-                    ,limit: 10
-                    ,page: {
-                        layout: ['count', 'prev', 'page', 'next', 'limit', 'refresh', 'skip']//自定义分页布局
-                        ,limits: [10,20,50,100]
-                        ,first: '首页'
-                        ,last: '尾页'
-                    }
-                    ,done : function(res, curr, count) {
-
-                        //监听搜索框事件
-                        $('.search .layui-btn').on('click', function(){
-                            let type = $(this).data('type');
-                            active[type] ? active[type].call(this) : '';
-                        });
-                        let active = {
-                            search: function(){
-                                datatable.reload({
-                                    where: {
-                                        'courseName': $(".search input[ name='courseName']").val()
-                                    }
-                                    ,page: {
-                                        curr: 1 //重新从第 1 页开始
-                                    }
-                                });
-                            }
-                            ,reset: function () {
-                                $(".search input").val('');
-                            }
-                        };
-
-                        //监听右侧工具条
-                        table.on('tool(datatable)', function(obj){
-                            if (obj.event === 'view') {
-                                layer.msg('查看评教', {time : 3000, offset: '100px'});
-                            } else  if (obj.event === 'pj') {
-                                //
-                                layer.open({
-                                    id: guid()
-                                    ,title : '教学评价-学生评教'
-                                    ,type : 1
-                                    ,area : [ '900px', '500px' ]
-                                    ,offset : '50px'
-                                    ,content : $('#editForm_container')
-                                    ,success: function(layero, index){
-                                        var templateCode;
-                                        var html = '';
-                                        $.each(result_data.data,function(idx,obj){
-                                            templateCode = obj.templateCode;
-                                            html += ' <div class="layui-form-item" style="margin-top: 20px" lay-verify="target">\n' +
-                                                (parseInt(idx)+1)+'，'+obj.targetContent+'<br/>' +
-                                                // '            <div class="layui-input-block">\n' +
-                                                '                <input type="radio" name="'+obj.targetCode+'" value="非常同意" title="非常同意">\n' +
-                                                '                <input type="radio" name="'+obj.targetCode+'" value="比较同意" title="比较同意">\n' +
-                                                '                <input type="radio" name="'+obj.targetCode+'" value="一般" title="一般">\n' +
-                                                '                <input type="radio" name="'+obj.targetCode+'" value="不太同意" title="不太同意">\n' +
-                                                '                <input type="radio" name="'+obj.targetCode+'" value="不同意" title="不同意">\n' +
-                                                // '            </div>\n' +
-                                                '        </div>';
-                                        });
-                                        html += '<textarea name="suggest" placeholder="您对本课程的建议" class="layui-textarea"></textarea>';
-                                        html += '<div class="layui-btn-container" style="margin-top: 20px" align="center">\n' +
-                                            '       <button class="layui-btn layui-btn-normal" lay-submit="" lay-filter="toSubmitEidtForm">保存</button>\n' +
-                                            '    </div>';
-                                        $("#editForm").html(html);
-                                        form.render('radio'); //刷新radio单选框框渲染
-
-                                        //自定义验证规则
-                                        form.verify({
-                                            target: function(value,element){
-                                                let num = 0;
-                                                if(!$(element).find(".layui-form-radio").hasClass("layui-form-radioed")){ //若有未选择的指标项
-                                                    $.each(result_data.data,function(idx,obj){
-                                                        if(!$("input[name="+obj.targetCode+"]").is(":checked")){ //则遍历出是哪一道题
-                                                            num = (parseInt(idx)+1);
-                                                            return false;//跳出循环
-                                                        }
-                                                    });
-                                                    return '您第'+num+'个问题没有回答';
-                                                }
-                                            }
-                                        });
-
-                                        //监听表单提交
-                                        form.on('submit(toSubmitEidtForm)', function(data){
-                                            $.post(requestUrl+'/xspj/insert.do' ,{
-                                                "courseCode" : obj.data.courseCode,
-                                                "templateCode" : templateCode
-                                                ,'userId':$.cookie('userId')
-                                                ,'userName':$.cookie('userName')
-                                                ,'userUnit':$.cookie('userUnit')
-                                                ,'jsonStr':JSON.stringify(data.field)
-                                            } ,function(result_data){
-                                                layer.msg(result_data.msg, { offset: '100px'}, function () {
-                                                    if(result_data.code == 200){
-                                                        datatable.reload();//重新加载表格数据
-                                                    }
-                                                    layer.close(index);
-                                                });
-                                            },'json');
-                                        });
-                                    }
-                                    ,cancel: function(index, layero){
-                                        layer.confirm('表单未提交，填写的信息将会清空？', {icon: 3, title:'提示', offset: '100px'}, function(index) {
-                                            layer.closeAll();
-                                        });
-                                        return false;
-                                    }
-                                });
-                            }
-                        });
-                    }
-                });
-
             }
+
+            //数据表格
+            let datatable = table.render({
+                id: guid() //设定一个id，防止重复弹出
+                ,elem : '#datatable'
+                ,height : 580
+                ,url: requestUrl+'/xspj/getPageList.do'
+                ,where:{
+                    "accountType":accountType,
+                    "userId":function () {
+                        return $.cookie('userId');
+                    }
+                }
+                ,request: {
+                    pageName: 'pageIndex'
+                    ,limitName: 'pageSize'
+                }
+                ,response: {
+                    statusCode: 200 //规定成功的状态码，默认：0
+                }
+                ,parseData: function(res){ //res 即为原始返回的数据
+                    return {
+                        "code": res.code, //解析接口状态
+                        "msg": res.msg, //解析提示文本
+                        "count": res.data.totalNum, //解析数据长度
+                        "data": res.data.pageList //解析数据列表
+                    };
+                }
+                ,cols : [[ //表头
+                    {type:'numbers', title:'序号', width:80, fixed: 'left'}
+                    ,{field:'courseName', title:'课程名称', width:180, sort:true, templet: function (data) {
+                            let html = '<a class="layui-btn layui-btn-disabled layui-btn-xs">已评</a>';
+                            if(data.isPj === 2){
+                                html = '<a class="layui-btn layui-btn-primary layui-btn-xs" lay-event="pj">未评</a>';
+                            }
+                            $('#datatable_bar').html(html);
+                            return data.courseName;
+                        }}
+                    ,{field:'courseAttr', title:'课程性质', width:150, sort:true}
+                    ,{field:'stuScore', title:'学分', width:150, sort:true}
+                    ,{field:'stuHour', title:'学时', width:150, sort:true}
+                    ,{field:'majorName', title:'适用专业', width:180, sort:true}
+                    ,{field:'collegeName', title:'开课学院', width:180, sort:true}
+                    ,{fixed: 'right', width:80, align:'center', toolbar: '#datatable_bar'}
+                ]]
+                ,even: true //隔行背景
+                ,limit: 10
+                ,page: {
+                    layout: ['count', 'prev', 'page', 'next', 'limit', 'refresh', 'skip']//自定义分页布局
+                    ,limits: [10,20,50,100]
+                    ,first: '首页'
+                    ,last: '尾页'
+                }
+                ,done : function(res, curr, count) {
+
+                    //监听搜索框事件
+                    $('.search .layui-btn').on('click', function(){
+                        let type = $(this).data('type');
+                        active[type] ? active[type].call(this) : '';
+                    });
+                    let active = {
+                        search: function(){
+                            datatable.reload({
+                                where: {
+                                    'courseName': $(".search input[ name='courseName']").val()
+                                }
+                                ,page: {
+                                    curr: 1 //重新从第 1 页开始
+                                }
+                            });
+                        }
+                        ,reset: function () {
+                            $(".search input").val('');
+                        }
+                    };
+
+                    //监听右侧工具条
+                    table.on('tool(datatable)', function(obj){
+                        if (obj.event === 'pj') {
+                            //
+                            layer.open({
+                                id: guid()
+                                ,title : '教学评价-学生评教'
+                                ,type : 1
+                                ,area : [ '900px', '500px' ]
+                                ,offset : '50px'
+                                ,content : $('#editForm_container')
+                                ,success: function(layero, index){
+                                    var html = '';
+                                    $.each(result_data.data,function(idx,obj){
+                                        html += ' <div class="layui-form-item" style="margin-top: 20px" lay-verify="target">\n' +
+                                            (parseInt(idx)+1)+'，'+obj.targetContent+'<br/>' +
+                                            // '            <div class="layui-input-block">\n' +
+                                            '                <input type="radio" name="'+obj.targetCode+'" value="非常同意" title="非常同意">\n' +
+                                            '                <input type="radio" name="'+obj.targetCode+'" value="比较同意" title="比较同意">\n' +
+                                            '                <input type="radio" name="'+obj.targetCode+'" value="一般" title="一般">\n' +
+                                            '                <input type="radio" name="'+obj.targetCode+'" value="不太同意" title="不太同意">\n' +
+                                            '                <input type="radio" name="'+obj.targetCode+'" value="不同意" title="不同意">\n' +
+                                            // '            </div>\n' +
+                                            '        </div>';
+                                    });
+                                    html += '<textarea name="suggest" placeholder="您对本课程的建议" class="layui-textarea"></textarea>';
+                                    html += '<div class="layui-btn-container" style="margin-top: 20px" align="center">\n' +
+                                        '       <button class="layui-btn layui-btn-normal" lay-submit="" lay-filter="toSubmitEidtForm">保存</button>\n' +
+                                        '    </div>';
+                                    $("#editForm").html(html);
+                                    form.render('radio'); //刷新radio单选框框渲染
+
+                                    //自定义验证规则
+                                    form.verify({
+                                        target: function(value,element){
+                                            let num = 0;
+                                            if(!$(element).find(".layui-form-radio").hasClass("layui-form-radioed")){ //若有未选择的指标项
+                                                $.each(result_data.data,function(idx,obj){
+                                                    if(!$("input[name="+obj.targetCode+"]").is(":checked")){ //则遍历出是哪一道题
+                                                        num = (parseInt(idx)+1);
+                                                        return false;//跳出循环
+                                                    }
+                                                });
+                                                return '您第'+num+'个问题没有回答';
+                                            }
+                                        }
+                                    });
+
+                                    //监听表单提交
+                                    form.on('submit(toSubmitEidtForm)', function(data){ // alert(JSON.stringify(data.field));
+                                        let suggest = data.field.suggest;
+                                        delete data.field.suggest; //JS 删除对象中的某个属性
+                                        $.post(requestUrl+'/xspj/insert.do' ,{
+                                            "templateCode" : result_data.data[0].templateCode
+                                            ,"courseCode" : obj.data.courseCode
+                                            ,"suggest" : suggest
+                                            ,'userId':$.cookie('userId')
+                                            ,'userName':$.cookie('userName')
+                                            ,'jsonString':JSON.stringify(data.field)
+                                        } ,function(result_data){
+                                            layer.msg(result_data.msg, { offset: '100px'}, function () {
+                                                if(result_data.code == 200){
+                                                    datatable.reload();//重新加载表格数据
+                                                }
+                                                layer.close(index);
+                                            });
+                                        },'json');
+                                    });
+                                }
+                                ,cancel: function(index, layero){
+                                    layer.confirm('表单未提交，填写的信息将会清空？', {icon: 3, title:'提示', offset: '100px'}, function(index) {
+                                        layer.closeAll();
+                                    });
+                                    return false;
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+
         },'json');
     } else{
 
